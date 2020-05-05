de8d0229d7f2a0634cf702ef36a8c7e282c6346f
Sushi Chanrai <sushi@pathworks.co>
2020-05-05 17:40:23 -0400

    create radio button answers group

--- src/index.js
+++ src/index.js
@@ -8,19 +8,28 @@ document.addEventListener('DOMContentLoaded', () => {
   const subPrompt = document.querySelector('#subprompt')
   const image = document.querySelector('img')
   const questionCounter = document.querySelector('#question-count')
+  const radioButtons = document.querySelector('#radio-buttons')
 
 
-  gameInitView()
+
 
 // INITIAL FETCH
   fetch(FISH_URL, { method: 'GET' })
     .then(resp => resp.json())
     .then(fishDataJSON => console.log(fishDataJSON));
 
+// RENDER DETAILS OF CLICKED DOG
+    dogBar.addEventListener('click', (e) => {
+      const clickedDogId = parseInt(e.target.dataset.id)
+      const foundDog = Dog.findDog(clickedDogId)
+      dogInfo.innerHTML = foundDog.renderDetails()
+    })
+
     function gameInitView(){
-      startContBtn.innerText = "Submit"
       mainPrompt.innerText = "What fish is this?"
       subPrompt.classList.add('hide')
+      startContBtn.classList.add('hide')
+      radioButtons.classList.remove('hide')
       //image.src = "https://myfwc.com/media/13792/bigeyescad.jpg"
 
     }
