e2d000cf166bd3acae1db0fc3715e89a3c851dd8
Sushi Chanrai <sushi@pathworks.co>
2020-05-05 20:24:06 -0400

    develop game initialize and initial fetch main functionality

--- src/index.js
+++ src/index.js
@@ -1,48 +1,53 @@
 const BACKEND_URL = 'http://localhost:3000';
 const FISH_URL = 'http://localhost:3000/api/v1/fish';
 
+const mainPrompt = document.querySelector('#prompt')
+const startContBtn = document.querySelector('#start-continue')
+const subPrompt = document.querySelector('#subprompt')
+const image = document.querySelector('img')
+const questionCounter = document.querySelector('#question-count')
+const radioButtons = document.querySelector('#radio-buttons')
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
-  const mainPrompt = document.querySelector('#prompt')
-  const startContBtn = document.querySelector('#start-continue')
-  const subPrompt = document.querySelector('#subprompt')
-  const image = document.querySelector('img')
-  const questionCounter = document.querySelector('#question-count')
-  const radioButtons = document.querySelector('#radio-buttons')
-
-
-
-
-// INITIAL FETCH
-  fetch(FISH_URL, { method: 'GET' })
-    .then(resp => resp.json())
-    .then(fishDataJSON => console.log(fishDataJSON));
-
-// RENDER DETAILS OF CLICKED DOG
-    dogBar.addEventListener('click', (e) => {
-      const clickedDogId = parseInt(e.target.dataset.id)
-      const foundDog = Dog.findDog(clickedDogId)
-      dogInfo.innerHTML = foundDog.renderDetails()
+// TRIGGER NEW GAME
+    startContBtn.addEventListener('click', () => {
+      let newGame = new Game()
     })
-
-    function gameInitView(){
-      mainPrompt.innerText = "What fish is this?"
-      subPrompt.classList.add('hide')
-      startContBtn.classList.add('hide')
-      radioButtons.classList.remove('hide')
-      //image.src = "https://myfwc.com/media/13792/bigeyescad.jpg"
-
-    }
-
 //DND-this ends the whole callback function for the DOMContentLoaded
 });
 
 
 
-// class Game {
-//
-//
-// }
+class Game {
+ constructor() {
+
+   this.score = 0
+   this.questionCounter = 1
+   this.gameFetch()
+   }
+
+// INITIAL FETCH
+  gameFetch(){
+   console.log("into the fetch function")
+   fetch(FISH_URL, { method: 'GET' })
+    .then(resp => resp.json())
+    .then(fishDataJSON => console.log(fishDataJSON))
+    this.gameInitRender()
+ }
+
+   gameInitRender(){
+    mainPrompt.innerText = "What fish is this?"
+    subPrompt.classList.add('hide')
+    startContBtn.classList.add('hide')
+    radioButtons.classList.remove('hide')
+    //image.src = "https://myfwc.com/media/13792/bigeyescad.jpg"
+  }
+
+}
+
+
 //
 // class Questions {
 //
